c22ab7243423bc2ccfd405622944ce02c46a5ebc
Darkcrow <Darkcrow@192.168.1.102>
2018-11-20 10:25:35 +0100

    Creamos los métodos set y get con excepción.

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
@@ -11,4 +11,42 @@ package org.iesalandalus.programacion.caballoajedrez;
 public class Posicion {
 	private int fila;
 	private char columna;
+
+/**
+ * Primero creamos el getFila.
+ * Devolverá un entero entre 1 y 8.
+ */
+public int getFila() {
+    return fila;
+}
+
+/**
+ * Creamos el getColumna.
+ * Devolverá un carácter entre a y h.
+ */
+public char getColumna() {
+    return columna;
+}
+
+/**
+ * Creamos el método setFila con una comprobación de valor.
+ */
+public void setFila(int fila) {
+    if (fila >= 1 && fila <= 8) {
+        this.fila = fila;
+    } else {
+    	throw new IllegalArgumentException("ERROR: Fila incorrecta.");
+    }
+}
+
+/**
+ * Creamos el método setColumna con una comprobación de valor.
+ */
+public void setColumna(char columna) {
+    if (columna >= 97 && columna <= 104) {
+        this.columna = columna;
+    } else {
+    	throw new IllegalArgumentException("ERROR: Columna incorrecta.");
+    }
+}
 }
