2c12cbd9b9c90a18a932e731f35a49c17303689f
Darkcrow <Darkcrow@192.168.1.102>
2018-11-20 10:16:21 +0100

    Creación clase Posición con atributos fila y columna.

---
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
@@ -0,0 +1,14 @@
+/**
+ * Creación de la clase Posición
+ */
+
+package org.iesalandalus.programacion.caballoajedrez;
+
+/**
+ * @author Emanuel Martínez Lonardi
+ *
+ */
+public class Posicion {
+	private int fila;
+	private char columna;
+}
