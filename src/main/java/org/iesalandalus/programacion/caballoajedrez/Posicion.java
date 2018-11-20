10210b9f16cf07bbec973cfae5799e08b32c65cc
Darkcrow <Darkcrow@192.168.1.102>
2018-11-20 11:22:25 +0100

    Creación de los métodos get y set con excepción.

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
@@ -9,44 +9,58 @@ package org.iesalandalus.programacion.caballoajedrez;
  *
  */
 public class Posicion {
-	private int fila;
-	private char columna;
 
-/**
- * Primero creamos el getFila.
- * Devolverá un entero entre 1 y 8.
- */
-public int getFila() {
-    return fila;
-}
+    private int fila;
+    private char columna;
 
-/**
- * Creamos el getColumna.
- * Devolverá un carácter entre a y h.
- */
-public char getColumna() {
-    return columna;
-}
-
-/**
- * Creamos el método setFila con una comprobación de valor.
- */
-public void setFila(int fila) {
-    if (fila >= 1 && fila <= 8) {
-        this.fila = fila;
-    } else {
-    	throw new IllegalArgumentException("ERROR: Fila incorrecta.");
+    /*
+     * Creamos el get y set fila
+     */
+    public int getFila() {
+        return this.fila;
     }
-}
+    
+    public void setFila(int fila) {
 
-/**
- * Creamos el método setColumna con una comprobación de valor.
- */
-public void setColumna(char columna) {
-    if (columna >= 97 && columna <= 104) {
-        this.columna = columna;
-    } else {
-    	throw new IllegalArgumentException("ERROR: Columna incorrecta.");
+        if (fila >= 1 && fila <= 8) {
+            this.fila = fila;
+        } else {
+            throw new IllegalArgumentException("ERROR: Fila no válida.");
+        }
+
     }
-}
+    
+    /*
+     * Creamos el get y set columna
+     */
+    public char getColumna() {
+        return this.columna;
+    }
+    
+    
+    public void setColumna(char columna) {
+        String letras = "aAbBcCdDeEfFgGhH";
+        boolean letraValida = false;
+
+        for (int i = 0; i < letras.length(); i++) {
+            if (columna == letras.charAt(i)) {
+                letraValida = true;
+            }
+        }
+        if (letraValida) {
+            this.columna = columna;
+        } else {
+            throw new IllegalArgumentException("ERROR: Columna no válida.");
+        }
+    }
+    
+    /**
+     * Iniciamos los atributos fila y columna
+     *
+     */
+    public Posicion(int fila, char columna) {
+        this.setFila(fila);
+        this.setColumna(columna);
+    }
+
 }
