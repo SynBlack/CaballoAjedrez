ff8615e8efcce300f89442d04563c4d8c42b3747
Darkcrow <Darkcrow@192.168.1.102>
2018-11-21 11:51:13 +0100

    Creamos los métodos GET y SET de los atributos color y posicion con su correspondiente visibilidad.

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
@@ -1,10 +1,10 @@
 /*
- * Creación de la clase Caballo
+ * Creación de la clase Caballo.
  */
 package org.iesalandalus.programacion.caballoajedrez;
 
 /**
- * @author Emanuel Martínez Lonardi
+ * @author Emanuel Martínez Lonardi.
  *
  */
 
@@ -14,4 +14,29 @@ public class Caballo {
 	private Posicion posicion;
 	
 	
+	//Creación de los métodos GET y SET del atributo color.
+	public Color getColor() {
+        return this.color;
+    }
+    
+    private void setColor(Color color) {
+        if (color != null) {
+            this.color = color;
+        } else {
+            throw new NullPointerException("ERROR: el color no puede ser nulo");
+        }
+    }
+    
+    // Creación de los métodos GET y SET del atributo posicion.
+    public Posicion getPosicion() {
+        return this.posicion;
+    }
+    
+    private void setPosicion(Posicion posicion) {
+        if (posicion != null) {
+            this.posicion = posicion;
+        } else {
+            throw new NullPointerException("ERROR: la posición no puede ser nula");
+        }
+    }
 }
