af7342e7ce3797befbd7cdbac8ae99cd388a2513
Darkcrow <Darkcrow@192.168.1.102>
2018-11-21 11:46:50 +0100

    Creación de la clase Caballo con los atributos color y posicion.

---
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
@@ -0,0 +1,17 @@
+/*
+ * Creación de la clase Caballo
+ */
+package org.iesalandalus.programacion.caballoajedrez;
+
+/**
+ * @author Emanuel Martínez Lonardi
+ *
+ */
+
+public class Caballo {
+
+	private Color color;
+	private Posicion posicion;
+	
+	
+}
