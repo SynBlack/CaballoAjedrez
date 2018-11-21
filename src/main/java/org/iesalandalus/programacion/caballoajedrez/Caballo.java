1af003eedf32d5d2723717a8bb44b2b82160ffa7
Darkcrow <Darkcrow@192.168.1.102>
2018-11-21 12:59:30 +0100

    Creación del método mover que realizará el movimiento si es válido. En caso contrario saltará excepción y no se modificará la posición del caballo.

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
@@ -3,6 +3,9 @@
  */
 package org.iesalandalus.programacion.caballoajedrez;
 
+// Importamos la excepción para que funcione.
+import javax.naming.OperationNotSupportedException;
+
 /**
  * @author Emanuel Martínez Lonardi.
  *
@@ -12,81 +15,143 @@ public class Caballo {
 
 	private Color color;
 	private Posicion posicion;
-	
+
 	// Creación del constructor por defecto
-	
+
 	public Caballo() {
-        this.color = Color.NEGRO;
-        this.posicion = new Posicion(8, 'b');
-    }	
-	
-	//Creación de los métodos GET y SET del atributo color.
+		this.color = Color.NEGRO;
+		this.posicion = new Posicion(8, 'b');
+	}
+
+	// Creación de los métodos GET y SET del atributo color.
 	public Color getColor() {
-        return this.color;
-    }
-    
-    private void setColor(Color color) {
-        if (color != null) {
-            this.color = color;
-        } else {
-            throw new NullPointerException("ERROR: el color no puede ser nulo");
-        }
-    }
-    
-    // Creación de los métodos GET y SET del atributo posicion.
-    public Posicion getPosicion() {
-        return this.posicion;
-    }
-    
-    private void setPosicion(Posicion posicion) {
-        if (posicion != null) {
-            this.posicion = posicion;
-        } else {
-            throw new NullPointerException("ERROR: la posición no puede ser nula");
-        }
-    }
-    
-    // Creamos el construtor para la clase que creará el caballo en dicho color.
-    
-    public Caballo(Color color) {
-    	this.setColor(color);
-    	
-    	switch (color) {
-    	case BLANCO:
-    		this.posicion = new Posicion(1, 'b');
-    		break;
-    	case NEGRO:
-    		this.posicion = new Posicion(8, 'b');
-    		break;
-    	}
-    }
-    
-    /* 
-     * Creamos el constructor para la clase que acepte como parámetros el color y columna inicial.
-     * La columna inicial debe ser la 'b' o la 'g' y creará un caballo del color dado y colocado en dicha
-     * columna y cuya fila será la 1 si es BLANCO y 8 si es NEGRO. 
-     */
-    
-    public Caballo(Color color, char columna) {
-    	this.setColor(color);
-    	int fila;
-    	
-    	if (color == Color.BLANCO) {
-    		fila = 1;
-    	} else {
-    		fila = 8;
-    	}
-    	
-    	switch (columna) {
-    		case 'b':
-    			this.posicion = new Posicion(fila, columna);
-    			break;
-    		case 'g':
-    			this.posicion = new Posicion(fila, columna);
-    			break;
-    		default:
-    			throw new IllegalArgumentException("ERROR: la columna no es correcta.");
-    	}
-    	
-    }
+		return this.color;
+	}
+
+	private void setColor(Color color) {
+		if (color != null) {
+			this.color = color;
+		} else {
+			throw new NullPointerException("ERROR: el color no puede ser nulo.");
+		}
+	}
+
+	// Creación de los métodos GET y SET del atributo posicion.
+	public Posicion getPosicion() {
+		return this.posicion;
+	}
+
+	private void setPosicion(Posicion posicion) {
+		if (posicion != null) {
+			this.posicion = posicion;
+		} else {
+			throw new NullPointerException("ERROR: la posición no puede ser nula.");
+		}
+	}
+
+	// Creamos el construtor para la clase que creará el caballo en dicho color.
+
+	public Caballo(Color color) {
+		this.setColor(color);
+
+		switch (color) {
+		case BLANCO:
+			this.posicion = new Posicion(1, 'b');
+			break;
+		case NEGRO:
+			this.posicion = new Posicion(8, 'b');
+			break;
+		}
+	}
+
+	/*
+	 * Creamos el constructor para la clase que acepte como parámetros el color y
+	 * columna inicial. La columna inicial debe ser la 'b' o la 'g' y creará un
+	 * caballo del color dado y colocado en dicha columna y cuya fila será la 1 si
+	 * es BLANCO y 8 si es NEGRO.
+	 */
+
+	public Caballo(Color color, char columna) {
+		this.setColor(color);
+		int fila;
+
+		if (color == Color.BLANCO) {
+			fila = 1;
+		} else {
+			fila = 8;
+		}
+
+		switch (columna) {
+		case 'b':
+			this.posicion = new Posicion(fila, columna);
+			break;
+		case 'g':
+			this.posicion = new Posicion(fila, columna);
+			break;
+		default:
+			throw new IllegalArgumentException("ERROR: la columna no es correcta.");
+		}
+	}
+
+	/*
+	 * Crear método mover que modificará la posición del mismo o, si no puede hacer
+	 * ese movimiento, lance una excepción y que no modifique la posición del
+	 * caballo.
+	 */
+
+	public void mover(Direccion direccion) throws OperationNotSupportedException {
+		String letras = "abcdefgh";
+		boolean movimientoValido = false;
+		int fila = this.posicion.getFila();
+		int columna = 0;
+
+		for (int i = 1; i <= letras.length(); i++) {
+			if (this.posicion.getColumna() == letras.charAt(i)) {
+				columna = i;
+			}
+		}
+
+		switch (direccion) {
+		case ARRIBA_IZQUIERDA:
+			columna = columna + 2;
+			fila = fila - 1;
+			break;
+		case ARRIBA_DERECHA:
+			columna = columna + 2;
+			fila = fila + 1;
+			break;
+		case DERECHA_ARRIBA:
+			columna = columna + 1;
+			fila = fila + 2;
+			break;
+		case DERECHA_ABAJO:
+			columna = columna - 1;
+			fila = fila + 2;
+			break;
+		case ABAJO_DERECHA:
+			columna = columna - 2;
+			fila = fila + 1;
+			break;
+		case ABAJO_IZQUIERDA:
+			columna = columna - 2;
+			fila = fila - 1;
+			break;
+		case IZQUIERDA_ARRIBA:
+			columna = columna + 1;
+			fila = fila - 2;
+			break;
+		case IZQUIERDA_ABAJO:
+			columna = columna - 1;
+			fila = fila - 2;
+			break;
+		default:
+			System.err.println("Ese movimiento no es válido, vuelva a intentarlo.");
+		}
+
+		if (fila < 1 || fila > 8 || columna < 1 || columna > 8) {
+			throw new OperationNotSupportedException("ERROR: Movimiento no válido.");
+		} else {
+			System.out.println("El movimiento ha sido válido y correcto.");
+		}
+	}
 }
